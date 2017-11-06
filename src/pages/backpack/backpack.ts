1b254c16ac8473e65663d630304c8f7752e6b780
TsukioAkira <meyer.jeffrey@hotmail.nl>
2017-11-06 09:25:25 +0100

    Added Modals and confirmations to the buttons on the Backpack page
    figured out how to work with comfirmation modals in Ionic Typescript now
    advancing

--- src/pages/backpack/backpack.ts
+++ src/pages/backpack/backpack.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-backpack',
@@ -7,8 +8,59 @@ import { NavController } from 'ionic-angular';
 })
 export class BackPackPage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public alertCtrl: AlertController) {
+    }
 
-  }
 
+    addBackpack() {
+      let addBackpack = this.alertCtrl.create({
+          title: "Add Backpack",
+          message: "Please enter the following requirements.",
+          inputs: [
+              {
+                  name: 'Backpack Name*',
+                  placeholder: 'enter backpack name here...'
+              },
+          ],
+          buttons: [
+              {
+                  text: 'Cancel',
+                  handler: data => {
+                      console.log('Cancel clicked');
+                  }
+              },
+              {
+                  text: 'Save',
+                  handler: data => {
+                      console.log('Saved clicked');
+                  }
+              }
+          ]
+      });
+      addBackpack.present();
+    }
+
+
+
+    deleteBackpack() {
+        let deleteBackpack = this.alertCtrl.create({
+            title: 'Delete Backpack',
+            message: "Are you sure you want to delete your Backpack?",
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Delete',
+                    handler: data => {
+                        console.log('Saved clicked');
+                    }
+                }
+            ]
+        });
+        deleteBackpack.present();
+    }
 }
