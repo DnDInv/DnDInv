e13919aa8c454ced902c6bc238592fe01893819b
TsukioAkira <meyer.jeffrey@hotmail.nl>
2017-11-17 11:01:45 +0100

    Fix

--- src/pages/inventory/inventory.ts
+++ src/pages/inventory/inventory.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController, NavController, NavParams } from 'ionic-angular';
 import { Storage } from "@ionic/storage";
-import { HomePage } from "../home/home";
 
 @Component({
   selector: 'page-inventory',
