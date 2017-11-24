4d1d05d3eb6795e03d0e734dd3eb99353699d66a
TsukioAkira <meyer.jeffrey@hotmail.nl>
2017-11-24 11:53:16 +0100

    created functionality for displaying items from a object
    Finally made it work :D

--- src/pages/standarditem/standarditem.ts
+++ src/pages/standarditem/standarditem.ts
@@ -1,15 +1,63 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
 
+//import { Http } from '@angular/http';
 
 @Component({
-  selector: 'page-standarditem',
-  templateUrl: 'standarditem.html',
+    selector: 'page-standarditem',
+    templateUrl: 'standarditem.html',
 })
 export class StandarditemPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    weapons = [
+        {
+            "strdweapId": 0,
+            "type": "Simple Melee Weapon",
+            "name": "Club",
+            "cost": "1 SP",
+            "damage": "1d4 bludgeoning",
+            "weight": "2 LB",
+            "properties": "Light",
+            "amount": ""
+        },
+        {
+            "strdweapId": 1,
+            "type": "Simple Melee Weapon ",
+            "name": "Hammer",
+            "cost": "2 SP",
+            "damage": "1d4 bludgeoning",
+            "weight": "4 LB",
+            "properties": "Medium",
+            "amount": ""
+        },
+        {
+            "strdweapId": 2,
+            "type": "Advanced Melee Weapon ",
+            "name": "LongSword",
+            "cost": "4 SP",
+            "damage": "1d4 bludgeoning",
+            "weight": "6 LB",
+            "properties": "IDK",
+            "amount": ""
+        }
+    ];
+    armor = [
+        {
+            "strdarmId": 0,
+            "type": "Light armor",
+            "name": "Padded",
+            "Cost": "5 GP",
+            "armor_class": "11 + dex modifier",
+            "strenghtreq": "",
+            "Stealth": "Disadvantage",
+            "weight": "8 lb",
+            "amount": ""
+        }
+    ];
 
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    }
+
 
 }
