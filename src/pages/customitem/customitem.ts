d64627a67cf4b111b884bab1c42450ff2f57ee4f
TsukioAkira <meyer.jeffrey@hotmail.nl>
2017-11-30 09:01:46 +0100

    changed alot of things
    1. delete background image
    2. dataflow on the custom item page so that every input in a field is
    being read and passed on in an object
    3. styled some minor issues with the custom item page
    4. fixed the standard and custom button in inventory page so they always
    appear next to eachother.
    5. try working on the creating item function
    6. after 5 hours of struggle finally got it to work right.

--- src/pages/customitem/customitem.ts
+++ src/pages/customitem/customitem.ts
@@ -1,19 +1,90 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Storage } from "@ionic/storage";
 
 
 @Component({
-  selector: 'page-customitem',
-  templateUrl: 'customitem.html',
+    selector: 'page-customitem',
+    templateUrl: 'customitem.html',
 })
 export class CustomitemPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    //backpack variable for the selected backpack
+    public backpack;
+    //item to sync all the objects in this.itemKey.
+    item: any = [];
+    //itemKey to set the items from the individual backpacks
+    itemKey;
+
+    //the itemName field.
+    itemName: any;
+    //the Type field.
+    type: any;
+    //the amount field.
+    amount: number;
+    //the weight field.
+    weight: number;
+    //the price field.
+    price: any;
+    //the attune checkbox.
+    attune: boolean;
+    //the mechanical field.
+    mechanical: any;
+    //the flavor field.
+    flavor: any;
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
+        this.itemName = "";
+        this.type = "";
+        this.amount = null;
+        this.weight = null;
+        this.price = "";
+        this.attune = false;
+        this.mechanical = "";
+        this.flavor = "";
+
+        //get the selected backpacks
+        this.backpack = this.navParams.get("backpack");
+        //console.log("last backpack: ", this.backpack.name);
+        //set backpack key items for the items in individual inventories.
+        this.itemKey = "item:" + this.backpack.name + this.backpack.HardLimit;
+        //console.log("itemKey: ", this.itemKey);
+    }
 
 
-  attunement(event) {
-    //console.log("checked: ", event.checked);
-  }
+    attunement(event) {
+        //console.log("checked: ", event.checked);
+    }
+
+
+
+         this.itemKey = "item: " + this.backpack.name + this.backpack.HardLimit;
+        // console.log("ItemKey: ", this.itemKey);
+
+        let data = {
+            "itemName": this.itemName,
+            "type": this.type,
+            "amount": this.amount,
+            "weight": this.weight,
+            "price": this.price,
+            "attune": this.attune,
+            "mechanical": this.mechanical,
+            "flavor": this.flavor
+        };
+
+        this.storage.get(this.itemKey).then((val) => {
+            // val.filter(x => {return x.name == data.name}).length() > 0
+            val.push(data);
+            //console.log(this.itemKey);
+            this.item = val;
+            //console.log("Val: ", val);
+            //console.log("Data: ", data);
+            // this.storage.remove(this.itemKey)
+            this.storage.set(this.itemKey, val);
+        }).catch((err) => {
+            this.storage.set(this.itemKey, [data]);
+            this.item = [data];
+        });
+    }
 
 }
--- src/pages/inventory/inventory.ts
+++ src/pages/inventory/inventory.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AlertController, NavController, NavParams } from 'ionic-angular';
 import { Storage } from "@ionic/storage";
 import { CustomitemPage } from "../customitem/customitem";
-import {StandarditemPage} from "../standarditem/standarditem";
+import { StandarditemPage } from "../standarditem/standarditem";
 
 @Component({
   selector: 'page-inventory',
@@ -10,33 +10,48 @@ import {StandarditemPage} from "../standarditem/standarditem";
 })
 export class InventoryPage {
 
+  //backpack variable for the selected backpack
   public backpack;
-  item: any = [];
+
+  //info array for getting all the backpacks
+  info = [];
+  //item to sync all the objects in this.itemKey and get them on the screen.
+  item = [];
+  //itemKey to get the items from the individual backpacks.
   itemKey;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,public storage: Storage) {
-    //get all the backpacks
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public storage: Storage) {
+    //get the selected backpack
     this.backpack = this.navParams.get('backpack');
     //set backpack key items for the items in individual inventories.
-    this.itemKey = 'items:' + this.backpack.name + this.backpack.HardLimit;
+    this.itemKey = 'item: ' + this.backpack.name + this.backpack.HardLimit;
     //get items in the selected backpack
-    this.storage.get(this.itemKey).then(val => {
+    this.storage.get(this.itemKey).then((val) => {
         this.item = val;
+    }).catch((err) => {
+
     });
   }
+
   //function for opening a item.
   openItem() {
-      //push works in a push page. :)
-      //this.navCtrl.push(HomePage);
+      //this.navCtrl.push(itemPage);
   }
 
   //Creating custom item
-  createcustomItem(index) {
-    this.navCtrl.push(CustomitemPage);
+  createcustomItem() {
+      //getting all backpacks
+      this.storage.get('backpacks').then((val) => {
+          this.info = val;
+          this.navCtrl.push(CustomitemPage, { backpack: this.backpack });
+
+      }).catch((err) => {
+          console.log("backpack not found!");
+      });
   }
 
   //adding items
-  addstandardItem(index) {
+  addstandardItem() {
     this.navCtrl.push(StandarditemPage);
   }
 
